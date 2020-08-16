a933c858edec54fcb2d20b261856ca2150800606
RishkoSergey <risko_sergey@mail.ru>
2020-08-16 22:16:48 +0300

    init + form page

---
+++ pages/chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello, {{ user.name }}</h1>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  head(){
+    return {
+      title: `Комната ${this.user.room}`
+    }
+  },
+  middleware: ['chat'],
+  computed: mapState(['user'])
+}
+</script>
