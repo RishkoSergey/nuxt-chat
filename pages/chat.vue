cfa973dbac70bb7b6e0f9d8f9db6da5f8d4c3855
RishkoSergey <risko_sergey@mail.ru>
2020-08-25 00:37:53 +0300

    chat page layout

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="message in messages" :key="message.text">
-        {{ message.text }}
-      </li>
-    </ul>
+  <div class="c-wrap">
+    <div class="c-chat">
+      <ul>
+        <Message 
+          v-for="message in messages" 
+          :key="message.text" 
+          :name="message.name"
+          :text="message.text"
+          owner
+        />
+      </ul>
+    </div>
+    <div class="c-form">
+      <ChatForm />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import Message from '../components/Message';
+import ChatForm from '../components/ChatForm';
 
 export default {
+  components: {
+    Message,
+    ChatForm
+  },
   head(){
     return {
       title: `Комната ${this.user.room}`
@@ -21,3 +36,31 @@ export default {
   computed: mapState(['user', 'messages'])
 }
 </script>
+
+<style scoped>
+.c-wrap {
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.c-form {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  height: 80px;
+  background: #212121;
+}
+
+.c-chat {
+  position: absolute;
+  bottom: 80px;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  overflow-y: auto;
+}
+</style>
